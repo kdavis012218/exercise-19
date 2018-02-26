5cd82dc58e3420b99288982d99d6d92baa341696
kdavis012218 <35358468+kdavis012218@users.noreply.github.com>
2018-02-25 22:51:20 -0500

    exercise 19

---
+++ exercise 19/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int one;
+            string input;
+            int sum;
+            Console.Write("Enter first number:");
+            int.Parse(Console.ReadLine());
+            Console.Write("Enter second number:");
+            int.Parse(Console.ReadLine());
+            for (sum = int one; int one++)
+            {
+                Console.Write("Answer " + int one + ":");
+                input = int.Parse(Console.ReadLine());
+                
+            
+            Console.WriteLine("Would you like to continue? y or n");
+            Console.ReadLine();
+
+            if input = "y"
+
+                {
+                    return;
+                }     
+                
+                }
+                { 
+
+                    else
+                    Console.ReadKey();
+        }
+                }
+}
